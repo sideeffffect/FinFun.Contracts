b56890f92adbe707a694704234461c5e3c01eb51
Ondra Pelech <ondra.pelech@gmail.com>
2017-04-28 01:16:50 +0200

    Material Obs

--- src/FinFun.Contracts/Contracts.fs
+++ src/FinFun.Contracts/Contracts.fs
@@ -1,5 +1,6 @@
 module FinFun.Contracts
 
+open System
 open System.Collections.Generic
 
 open FSharpx.Functional
@@ -15,62 +16,108 @@ type Currency =
     | GBP
 
 
-type TimeStep = uint32
-
-
 type Date =
-    { instant : Instant
-      timeStep : uint32
-    }
-    static member from(s) = {instant = Instant.FromUnixTimeSeconds 0L; timeStep = s}
+    inherit IComparable<Date>
+    type DateFactory<'d when 'd :> Date> =
+        abstract member time0 : Date
 
 
-let time0() : Date = Date.from 0u
+type RV<'a when 'a : comparison> =
+    inherit IEquatable<RV<'a>>
 
 
-type RV<'a> = 'a[]
+type PR<'a when 'a : comparison> =
+    inherit IEquatable<PR<'a>>
+    abstract member Item : Date -> RV<'a>
 
 
-type PR<'a> = { unPr : (RV<'a>)[] } 
+// Observables primitives
 
 
-type Obs<'a> = Obs of IReadOnlyDictionary<Date, PR<'a>> with
-    override this.ToString() =
-        match this with
-        | Obs f -> let x = f.[time0()]
-                   sprintf "(Obs %A)" x.unPr.[0]
+[<NoEquality;NoComparison>]
+type Obs<'a> =
+    | Konst of 'a
+    | Map of (obj -> 'a) * Obs<obj>
+    | Lift2 of (obj -> obj -> 'a) * Obs<obj> * Obs<obj>
+    | Date
+    | Negate of Obs<'a> * Obs<'a>
+    | Plus of Obs<'a> * Obs<'a>
+    | Minus of Obs<'a> * Obs<'a>
+    | Mult of Obs<'a> * Obs<'a>
+    | Div of Obs<'a> * Obs<'a>
+    | Eq of Obs<obj> * Obs<obj>
+    | Ne of Obs<'a> * Obs<'a>
+    | Lt of Obs<'a> * Obs<'a>
+    | Le of Obs<'a> * Obs<'a>
+    | Gt of Obs<'a> * Obs<'a>
+    | Ge of Obs<'a> * Obs<'a>
 
 
+[<AutoOpen>]
+module Obs =
+    let konst : 'a -> Obs<'a> = Konst
+
+    let map (f : 'a -> 'b) (x : Obs<'a>) : Obs<'b> = Map (f, x)
+
+    let lift2 (f : 'a -> 'b -> 'c) (x : Obs<'a>) (y : Obs<'b>) : Obs<'c> = Lift2 (f, x, y)
+
+    let date : Obs<'a> = Date
+
+    let (+) (x : Obs<'a>) (y : Obs<'a>) : Obs<'a> = Plus (x, y)
+
+    let (-) (x : Obs<'a>) (y : Obs<'a>) : Obs<'a> = Minus (x, y)
+
+    let (*) (x : Obs<'a>) (y : Obs<'a>) : Obs<'a> = Mult (x, y)
+
+    let (/) (x : Obs<'a>) (y : Obs<'a>) : Obs<'a> = Div (x, y)
+
+    let (==) (x : Obs<'a>) (y : Obs<'a>) : Obs<bool> = Eq (x, y)
+
+    let (<) (x : Obs<'a>) (y : Obs<'a>) : Obs<bool> = Lt (x, y)
+
+    let (<=) (x : Obs<'a>) (y : Obs<'a>) : Obs<bool> = Le (x, y)
+
+    let (>) (x : Obs<'a>) (y : Obs<'a>) : Obs<bool> = Gt (x, y)
+
+    let (>=) (x : Obs<'a>) (y : Obs<'a>) : Obs<bool> = Ge (x, y)
+
+
+// Contract primitives
+
+[<NoEquality;NoComparison>]
 type Contract =
     | Zero
     | One  of Currency
     | Give of Contract
     | And  of Contract * Contract
     | Or   of Contract * Contract
-    | Cond    of (Obs<bool>)  * Contract * Contract
-    | Scale   of (Obs<float>) * Contract
-    | When    of (Obs<bool>)  * Contract
-    | Anytime of (Obs<bool>)  * Contract
-    | Until   of (Obs<bool>)  * Contract
+    | Cond    of Obs<bool>  * Contract * Contract
+    | Scale   of Obs<float> * Contract
+    | When    of Obs<bool>  * Contract
+    | Anytime of Obs<bool>  * Contract
+    | Until   of Obs<bool>  * Contract
 
 
-let zero : Contract= Zero
+[<AutoOpen>]
+module Contract =
+    let zero : Contract= Zero
 
-let one : Currency -> Contract = One
+    let one : Currency -> Contract = One
 
-let give : Contract -> Contract = Give
+    let give : Contract -> Contract = Give
 
-let and' : Contract -> Contract -> Contract = curry And
+    let and' : Contract -> Contract -> Contract = curry And
 
-let or' : Contract -> Contract -> Contract = curry Or
+    let or' : Contract -> Contract -> Contract = curry Or
 
-let cond : Obs<bool> -> Contract -> Contract -> Contract = curry3 Cond
+    let cond : Obs<bool> -> Contract -> Contract -> Contract = curry3 Cond
 
-let scale : Obs<float> -> Contract -> Contract = curry Scale
+    let scale : Obs<float> -> Contract -> Contract = curry Scale
 
-let when' : Obs<bool> -> Contract -> Contract = curry When
+    let when' : Obs<bool> -> Contract -> Contract = curry When
 
-let anytime : Obs<bool> -> Contract -> Contract = curry Anytime
+    let anytime : Obs<bool> -> Contract -> Contract = curry Anytime
 
-let until : Obs<bool> -> Contract -> Contract = curry Until
+    let until : Obs<bool> -> Contract -> Contract = curry Until
+
 
