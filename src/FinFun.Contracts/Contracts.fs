f432af792b3d859d34bcdc616b3f23645eb9f040
Ondra Pelech <ondra.pelech@gmail.com>
2017-04-28 23:10:44 +0200

    Implement RV, PR and Obs

--- src/FinFun.Contracts/Contracts.fs
+++ src/FinFun.Contracts/Contracts.fs
@@ -1,8 +1,5 @@
 module FinFun.Contracts
 
-open System
-open System.Collections.Generic
-
 open FSharpx.Functional
 open NodaTime
 
@@ -17,72 +14,118 @@ type Currency =
 
 
 type Date =
-    inherit IComparable<Date>
-    type DateFactory<'d when 'd :> Date> =
-        abstract member time0 : Date
+    { instant : Instant
+      timeStep : uint32
+    }
 
 
-type RV<'a when 'a : comparison> =
-    inherit IEquatable<RV<'a>>
+let date0() : Date =
+    { instant  = undefined
+      timeStep = 0u
+    }
 
 
-type PR<'a when 'a : comparison> =
-    inherit IEquatable<PR<'a>>
-    abstract member Item : Date -> RV<'a>
+// Random variable
+
+type RV<'a when 'a : comparison> = private RV of 'a[]
+
+[<AutoOpen>]
+module RV =
+    let map (f : 'a -> 'b) (x:RV<'a>) : RV<'b> =
+        match x with | RV a -> RV <| Array.map f a
+
+    let map2 (f : 'a -> 'b -> 'c) (x:RV<'a>) (y:RV<'b>) : RV<'c> =
+        match x, y with | RV a, RV b -> RV <| Array.map2 f a b
+
+    let map3 (f : 'a -> 'b -> 'c -> 'd) (x:RV<'a>) (y:RV<'b>) (z:RV<'c>) : RV<'d> =
+        match x, y, z with | RV a, RV b, RV c -> RV <| Array.map3 f a b c
 
 
-// Observables primitives
-
+// Process
 
 [<NoEquality;NoComparison>]
-type Obs<'a> =
-    | Konst of 'a
-    | Map of (obj -> 'a) * Obs<obj>
-    | Lift2 of (obj -> obj -> 'a) * Obs<obj> * Obs<obj>
-    | Date
-    | Negate of Obs<'a> * Obs<'a>
-    | Plus of Obs<'a> * Obs<'a>
-    | Minus of Obs<'a> * Obs<'a>
-    | Mult of Obs<'a> * Obs<'a>
-    | Div of Obs<'a> * Obs<'a>
-    | Eq of Obs<obj> * Obs<obj>
-    | Ne of Obs<'a> * Obs<'a>
-    | Lt of Obs<'a> * Obs<'a>
-    | Le of Obs<'a> * Obs<'a>
-    | Gt of Obs<'a> * Obs<'a>
-    | Ge of Obs<'a> * Obs<'a>
+type PR<'a when 'a : comparison> = private PR of seq<RV<'a>> with
+    member this.Item (d:Date) : RV<'a> =
+        match this with
+        | PR s -> Seq.item (int d.timeStep) s
+
+
+[<AutoOpen>]
+module PR =
+    let konst (x:'a) : PR<'a> =
+        let rec nextSlice count = seq {
+            yield RV <| Array.create count x
+            yield! nextSlice (count+1)
+        }
+        PR <| nextSlice 1
+
+    let dateFrom (d:Date) : PR<Date> =
+        let rec timeSlices count date = seq {
+            yield RV <| Array.create count date
+            yield! timeSlices (count+1) {timeStep = date.timeStep + 1u; instant = date.instant.Plus(Duration.FromDays 1) }
+        }
+        PR <| timeSlices 1 d
+
+    let date() = dateFrom <| date0()
+
+    let map (f : 'a -> 'b) (x:PR<'a>) : PR<'b> =
+        match x with PR y -> PR <| Seq.map (RV.map f) y
+
+    let map2 (f : 'a -> 'b -> 'c) (x:PR<'a>) (y:PR<'b>) : PR<'c> =
+        match x, y with PR a, PR b -> PR <| Seq.map2 (RV.map2 f) a b
+
+    let map3 (f : 'a -> 'b -> 'c -> 'd) (x:PR<'a>) (y:PR<'b>) (z:PR<'c>) : PR<'d> =
+        match x, y, z with PR a, PR b, PR c -> PR <| Seq.map3 (RV.map3 f) a b c
+
+    let cond (b:PR<bool>) (tru:PR<'a>) (fls:PR<'a>) : PR<'a> =
+        map3 (fun i t e -> if i then t else e) b tru fls
+
+    let inline plus  x y = map2 (fun a b -> a +  b) x y
+    let inline minus x y = map2 (fun a b -> a -  b) x y
+    let inline mult  x y = map2 (fun a b -> a *  b) x y
+    let inline div   x y = map2 (fun a b -> a /  b) x y
+    let inline eq    x y = map2 (fun a b -> a =  b) x y
+    let inline ne    x y = map2 (fun a b -> a <> b) x y
+    let inline lt    x y = map2 (fun a b -> a <  b) x y
+    let inline le    x y = map2 (fun a b -> a <= b) x y
+    let inline gt    x y = map2 (fun a b -> a >  b) x y
+    let inline ge    x y = map2 (fun a b -> a >= b) x y
+
+
+// Observable
+
+[<NoEquality;NoComparison>]
+type Obs<'a when 'a : comparison> = private Obs of (Date -> PR<'a>) with
+    member this.Item (d:Date) : PR<'a> =
+        match this with
+        | Obs s -> s d
 
 
 [<AutoOpen>]
 module Obs =
-    let konst : 'a -> Obs<'a> = Konst
+    let konst (x: 'a) : Obs<'a> = Obs (fun _ -> PR.konst x)
 
-    let map (f : 'a -> 'b) (x : Obs<'a>) : Obs<'b> = Map (f, x)
+    let map (f : 'a -> 'b) (x : Obs<'a>) : Obs<'b> =
+        match x with | Obs a -> Obs <| fun d -> PR.map f (a d)
 
-    let lift2 (f : 'a -> 'b -> 'c) (x : Obs<'a>) (y : Obs<'b>) : Obs<'c> = Lift2 (f, x, y)
+    let map2 (f : 'a -> 'b -> 'c) (x : Obs<'a>) (y : Obs<'b>) : Obs<'c> =
+        match x, y with | Obs a, Obs b -> Obs <| fun d -> PR.map2 f (a d) (b d)
 
-    let date : Obs<'a> = Date
+    let date : Obs<Date> = Obs <| dateFrom
 
-    let (+) (x : Obs<'a>) (y : Obs<'a>) : Obs<'a> = Plus (x, y)
-
-    let (-) (x : Obs<'a>) (y : Obs<'a>) : Obs<'a> = Minus (x, y)
-
-    let (*) (x : Obs<'a>) (y : Obs<'a>) : Obs<'a> = Mult (x, y)
-
-    let (/) (x : Obs<'a>) (y : Obs<'a>) : Obs<'a> = Div (x, y)
-
-    let (==) (x : Obs<'a>) (y : Obs<'a>) : Obs<bool> = Eq (x, y)
-
-    let (<) (x : Obs<'a>) (y : Obs<'a>) : Obs<bool> = Lt (x, y)
-
-    let (<=) (x : Obs<'a>) (y : Obs<'a>) : Obs<bool> = Le (x, y)
-
-    let (>) (x : Obs<'a>) (y : Obs<'a>) : Obs<bool> = Gt (x, y)
-
-    let (>=) (x : Obs<'a>) (y : Obs<'a>) : Obs<bool> = Ge (x, y)
+    let inline plus  x y = map2 (fun a b -> a +  b) x y
+    let inline minus x y = map2 (fun a b -> a -  b) x y
+    let inline mult  x y = map2 (fun a b -> a *  b) x y
+    let inline div   x y = map2 (fun a b -> a /  b) x y
+    let inline eq    x y = map2 (fun a b -> a =  b) x y
+    let inline ne    x y = map2 (fun a b -> a <> b) x y
+    let inline lt    x y = map2 (fun a b -> a <  b) x y
+    let inline le    x y = map2 (fun a b -> a <= b) x y
+    let inline gt    x y = map2 (fun a b -> a >  b) x y
+    let inline ge    x y = map2 (fun a b -> a >= b) x y
 
 
-// Contract primitives
+// Contract
 
 [<NoEquality;NoComparison>]
 type Contract =
@@ -100,24 +143,24 @@ type Contract =
 
 [<AutoOpen>]
 module Contract =
-    let zero : Contract= Zero
+    let zero : Contract = Zero
 
-    let one : Currency -> Contract = One
+    let one  : Currency -> Contract = One
 
     let give : Contract -> Contract = Give
 
     let and' : Contract -> Contract -> Contract = curry And
 
-    let or' : Contract -> Contract -> Contract = curry Or
+    let or'  : Contract -> Contract -> Contract = curry Or
 
-    let cond : Obs<bool> -> Contract -> Contract -> Contract = curry3 Cond
+    let cond    : Obs<bool>  -> Contract -> Contract -> Contract = curry3 Cond
 
-    let scale : Obs<float> -> Contract -> Contract = curry Scale
+    let scale   : Obs<float> -> Contract -> Contract             = curry Scale
 
-    let when' : Obs<bool> -> Contract -> Contract = curry When
+    let when'   : Obs<bool>  -> Contract -> Contract             = curry When
 
-    let anytime : Obs<bool> -> Contract -> Contract = curry Anytime
+    let anytime : Obs<bool>  -> Contract -> Contract             = curry Anytime
 
-    let until : Obs<bool> -> Contract -> Contract = curry Until
+    let until   : Obs<bool>  -> Contract -> Contract             = curry Until
 
 
